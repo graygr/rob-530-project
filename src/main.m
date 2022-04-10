13e936a6933a495055895d4d37902086342f7044
camkab <camkab@umich.edu>
2022-04-10 16:40:32 -0400

    add main update to read in filepath automatically

--- src/main.m
+++ src/main.m
@@ -1,18 +1,30 @@
 % File: main.m
-% Author: Gregor Limstrom <limstrom@umich.edu>
+% Author: Gregor Limstrom <limstrom@umich.edu>, 
+% Cameron Kabacinski <camkab@umich.ed>
 % Purpose: Open data and explore format for 530 project
 
-%% Import data from MRCLAM_9 - Defunct, they provide load and sample code
+clc;
+close all;
+clearvars -except mainFilePath 
 % Set pwd
-% TODO: Change to match your filepath
-cd("C:\Users\akgre\Documents\masters\NAVARCH 568\project\")
-
+if ~exist('mainFilePath')
+    mainFilePath = pwd;
+end
+% change directory to top level so we can access data
+cd("../")
+%% Import data from MRCLAM_9 - Defunct, they provide load and sample code
 % Load data
-run('data\MRCLAM9\loadMRCLAMdataSet.m')
+% run('data\MRCLAM9\loadMRCLAMdataSet.m')
+run('data\MRCLAM1\loadMRCLAMdataSet.m')
 
 % Sample data
 run('Tools\sampleMRCLAMdataSet.m')
 
+%% Animate data
+%run('Tools\animateMRCLAMdataSet.m')
+
+ 
+cd(mainFilePath)
 %% Particle filter for localization
 
 % Update based on measurements of landmarks and other robots
